e76ad65fc04c576f086a706f80629baf8cb36068
blockbrossnakegame <159715270+blockbrossnakegame@users.noreply.github.com>
2025-01-04 17:26:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ client3.on("ready", () => {
 app.get("/getmsg", async (req, res) => {
   const channel = client.channels.cache.get("1311381333406257163")
   const lastMessage = await channel.messages.fetch({ limit: 1 });
-  res.send(`${lastMessage.author.username} ${lastMessage.content}`);
+  res.send(`${lastMessage.content}`);
 })
 
 app.get('/', (req, res) => {
